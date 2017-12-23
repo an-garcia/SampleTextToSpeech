13e8b3b57e7215c10f859356e19641b68525c3be
Angel Newton <xanewton@gmail.com>
2017-12-23 15:36:31 -0500

    Add sample text and button to view

--- SampleTextToSpeech/ViewController.swift
+++ SampleTextToSpeech/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // https://www.hackingwithswift.com/example-code/media/how-to-convert-text-to-speech-using-avspeechsynthesizer-avspeechutterance-and-avspeechsynthesisvoice
+        label.text = "In the beginning there was darkeness."
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Speak action
+    @IBAction func spek(_ sender: Any) {
+        
+    }
 }
 
